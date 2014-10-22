60b5a6976302549871e3bbd8e8944dd9cb796380
marx <marx@cern.ch>
2014-10-22 14:10:20 +0000

    adding more branches

--- xAODAthena/btagIBLAnalysis/src/btagIBLAnalysisAlg.h
+++ xAODAthena/btagIBLAnalysis/src/btagIBLAnalysisAlg.h
@@ -1,12 +1,12 @@
 #ifndef BTAGIBLANALYSIS_BTAGIBLANALYSISALG_H
 #define BTAGIBLANALYSIS_BTAGIBLANALYSISALG_H 1
 
-#include "AthenaBaseComps/AthAlgorithm.h"
+#include "AthenaBaseComps/AthHistogramAlgorithm.h"
 
 #include "TFile.h"
 #include "TTree.h"
 
-class btagIBLAnalysisAlg: public ::AthAlgorithm { 
+class btagIBLAnalysisAlg: public ::AthHistogramAlgorithm { 
  public: 
   btagIBLAnalysisAlg( const std::string& name, ISvcLocator* pSvcLocator );
   virtual ~btagIBLAnalysisAlg(); 
@@ -26,6 +26,37 @@ class btagIBLAnalysisAlg: public ::AthAlgorithm {
   std::vector<float> *v_jet_pt;
   std::vector<float> *v_jet_eta;
   std::vector<float> *v_jet_phi;
+  std::vector<int> *v_jet_truthflav;
+
+  std::vector<double> *v_jet_ip3d_pb;
+  std::vector<double> *v_jet_ip3d_pc;
+  std::vector<double> *v_jet_ip3d_pu;
+  std::vector<double> *v_jet_ip3d_llr;
+
+  std::vector<double> *v_jet_sv1_pb;
+  std::vector<double> *v_jet_sv1_pc;
+  std::vector<double> *v_jet_sv1_pu;
+  std::vector<double> *v_jet_sv1_llr;
+
+  std::vector<double> *v_jet_jf_pb;
+  std::vector<double> *v_jet_jf_pc;
+  std::vector<double> *v_jet_jf_pu;
+  std::vector<double> *v_jet_jf_llr;
+
+  std::vector<double> *v_jet_jfcombnn_pb;
+  std::vector<double> *v_jet_jfcombnn_pc;
+  std::vector<double> *v_jet_jfcombnn_pu;
+  std::vector<double> *v_jet_jfcombnn_llr;
+
+  std::vector<double> *v_jet_sv1ip3d_discr;
+  std::vector<double> *v_jet_mv1_discr;
+
+  std::vector<float> *v_bH_pt;
+  std::vector<float> *v_bH_eta;
+  std::vector<float> *v_bH_phi;
+  std::vector<float> *v_bH_Lxy;
+  //std::vector<float> *v_jet_Lxy_denom;
+
 }; 
 
 #endif //> !BTAGIBLANALYSIS_BTAGIBLANALYSISALG_H
--- xAODAthena/run/jobOptions.py
+++ xAODAthena/run/jobOptions.py
@@ -3,18 +3,51 @@
 # ====================================================================
 theApp.EvtMax = -1
 import AthenaPoolCnvSvc.ReadAthenaPool
+mainfolder = "/afs/cern.ch/work/m/marx/btag/"
+foldername = mainfolder + "ttbar/13TeV/mc14_13TeV.110401.PowhegPythia_P2012_ttbar_nonallhad.recon.AOD.e2928_s1982_s2008_r5720_tid01529511_00/"
+#foldername = mainfolder + "Zprime/13TeV/mc14_13TeV.110903.Pythia8_AU2MSTW2008LO_zprime1000_tt.merge.AOD.e2743_s1982_s2008_r5787_r5853_tid01598032_00/"
 svcMgr.EventSelector.InputCollections = [
     #"/afs/cern.ch/user/m/marx/btag/xAODAthena/run/myxAOD.pool.root",
-    "/afs/cern.ch/work/m/marx/btag/ttbar/13TeV/mc14_13TeV.110401.PowhegPythia_P2012_ttbar_nonallhad.recon.AOD.e2928_s1982_s2008_r5720_tid01529511_00/AOD.01529511._000183.pool.root.2",
-    ## "/afs/cern.ch/work/m/marx/btag/ttbar/13TeV/mc14_13TeV.110401.PowhegPythia_P2012_ttbar_nonallhad.recon.AOD.e2928_s1982_s2008_r5720_tid01529511_00/AOD.01529511._000356.pool.root.1",
-    ## "/afs/cern.ch/work/m/marx/btag/ttbar/13TeV/mc14_13TeV.110401.PowhegPythia_P2012_ttbar_nonallhad.recon.AOD.e2928_s1982_s2008_r5720_tid01529511_00/AOD.01529511._000551.pool.root.2",
-    ## "/afs/cern.ch/work/m/marx/btag/ttbar/13TeV/mc14_13TeV.110401.PowhegPythia_P2012_ttbar_nonallhad.recon.AOD.e2928_s1982_s2008_r5720_tid01529511_00/AOD.01529511._000592.pool.root.1",
-    ## "/afs/cern.ch/work/m/marx/btag/ttbar/13TeV/mc14_13TeV.110401.PowhegPythia_P2012_ttbar_nonallhad.recon.AOD.e2928_s1982_s2008_r5720_tid01529511_00/AOD.01529511._000603.pool.root.1",
-    ## "/afs/cern.ch/work/m/marx/btag/ttbar/13TeV/mc14_13TeV.110401.PowhegPythia_P2012_ttbar_nonallhad.recon.AOD.e2928_s1982_s2008_r5720_tid01529511_00/AOD.01529511._000858.pool.root.1",
-    ## "/afs/cern.ch/work/m/marx/btag/ttbar/13TeV/mc14_13TeV.110401.PowhegPythia_P2012_ttbar_nonallhad.recon.AOD.e2928_s1982_s2008_r5720_tid01529511_00/AOD.01529511._000961.pool.root.1",
-    ## "/afs/cern.ch/work/m/marx/btag/ttbar/13TeV/mc14_13TeV.110401.PowhegPythia_P2012_ttbar_nonallhad.recon.AOD.e2928_s1982_s2008_r5720_tid01529511_00/AOD.01529511._001032.pool.root.1",
-    ## "/afs/cern.ch/work/m/marx/btag/ttbar/13TeV/mc14_13TeV.110401.PowhegPythia_P2012_ttbar_nonallhad.recon.AOD.e2928_s1982_s2008_r5720_tid01529511_00/AOD.01529511._001663.pool.root.1",
-    ## "/afs/cern.ch/work/m/marx/btag/ttbar/13TeV/mc14_13TeV.110401.PowhegPythia_P2012_ttbar_nonallhad.recon.AOD.e2928_s1982_s2008_r5720_tid01529511_00/AOD.01529511._001985.pool.root.1"
+    #foldername + "AOD.01598032._000007.pool.root.1",
+    #foldername + "AOD.01598032._000008.pool.root.1",
+    #foldername + "AOD.01598032._000097.pool.root.1",
+    #foldername + "AOD.01598032._000151.pool.root.1",
+    foldername + "AOD.01529511._000075.pool.root.1",
+    #foldername + "AOD.01529511._000109.pool.root.1",
+    #foldername + "AOD.01529511._000112.pool.root.1",
+    #foldername + "AOD.01529511._000183.pool.root.2",
+    #foldername + "AOD.01529511._000226.pool.root.1",
+    #foldername + "AOD.01529511._000302.pool.root.1",
+    #foldername + "AOD.01529511._000338.pool.root.1",
+    #foldername + "AOD.01529511._000356.pool.root.1",
+    #foldername + "AOD.01529511._000450.pool.root.3",
+    #foldername + "AOD.01529511._000551.pool.root.2",
+    #foldername + "AOD.01529511._000592.pool.root.1",
+    #foldername + "AOD.01529511._000603.pool.root.1",
+    #foldername + "AOD.01529511._000659.pool.root.1",
+    #foldername + "AOD.01529511._000699.pool.root.1",
+    #foldername + "AOD.01529511._000747.pool.root.1",
+    #foldername + "AOD.01529511._000762.pool.root.1",
+    #foldername + "AOD.01529511._000836.pool.root.1",
+    #foldername + "AOD.01529511._000856.pool.root.1",
+    #foldername + "AOD.01529511._000858.pool.root.1",
+    #foldername + "AOD.01529511._000875.pool.root.1",
+    #foldername + "AOD.01529511._000961.pool.root.2",
+    #foldername + "AOD.01529511._001032.pool.root.1",
+    #foldername + "AOD.01529511._001070.pool.root.5",
+    #foldername + "AOD.01529511._001181.pool.root.1",
+    #foldername + "AOD.01529511._001297.pool.root.1",
+    #foldername + "AOD.01529511._001394.pool.root.1",
+    #foldername + "AOD.01529511._001456.pool.root.1",
+    #foldername + "AOD.01529511._001493.pool.root.2",
+    #foldername + "AOD.01529511._001553.pool.root.1",
+    #foldername + "AOD.01529511._001594.pool.root.2",
+    #foldername + "AOD.01529511._001614.pool.root.2",
+    #foldername + "AOD.01529511._001663.pool.root.1",
+    #foldername + "AOD.01529511._001664.pool.root.2",
+    #foldername + "AOD.01529511._001895.pool.root.1",
+    #foldername + "AOD.01529511._001944.pool.root.2",
+    #foldername + "AOD.01529511._001985.pool.root.1"
      ]
 # ====================================================================
 # Define output file name and stream name
