1920ffc6e44d8ff14de0ed53ff7989debd80fcc8
marx <marx@cern.ch>
2014-10-25 11:35:20 +0000

    grid stuff

---
+++ xAODAthena/run/prun.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+inputs=mc_samples.txt
+
+cat $inputs | while read inDataSet
+  do
+ 
+  if [[ "${inDataSet}" == "#"* ]];
+      then
+      #echo skipping ${inDataSet}
+      continue
+  fi
+  outDataSet=user.marx.test.flavntuplev6
+
+
+
+  echo -e "\n \n"
+  echo dataset= $inDataSet
+  echo out=$outDataSet
+ 
+
+##group version version
+  prun --exec "echo %IN | sed 's/,/\n/g' > input.txt; athena jobOptions.py" --athenaTag=AthAnalysisBase,2.0.14 --noBuild --inDS ${inDataSet} --outDS ${outDataSet} --nFilesPerJob 1 --excludedSite ANALY_BHAM_SL6,ANALY_ECDF_SL6
+#--site BNL-OSG2_DATADISK
+
+
+
+done
+
+
+
+
+
