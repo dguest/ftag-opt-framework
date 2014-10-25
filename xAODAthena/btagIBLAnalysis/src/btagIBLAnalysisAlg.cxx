f618aff3da3f5d7578054adfab31009cf77e6305
marx <marx@cern.ch>
2014-10-25 11:33:10 +0000

    now runs on 8TeV samples as well

--- xAODAthena/btagIBLAnalysis/src/btagIBLAnalysisAlg.cxx
+++ xAODAthena/btagIBLAnalysis/src/btagIBLAnalysisAlg.cxx
@@ -401,11 +401,16 @@ StatusCode btagIBLAnalysisAlg::execute() {
     // Other
     v_jet_sv1ip3d->push_back(bjet->SV1plusIP3D_discriminant());
     v_jet_mv1    ->push_back(bjet->MV1_discriminant());
-    v_jet_mv1c   ->push_back(bjet->auxdata<double>("MV1c_discriminant"));
-    v_jet_mv2c00 ->push_back(bjet->auxdata<double>("MV2c00_discriminant"));
-    v_jet_mv2c10 ->push_back(bjet->auxdata<double>("MV2c10_discriminant"));
-    v_jet_mv2c20 ->push_back(bjet->auxdata<double>("MV2c20_discriminant"));
-    v_jet_mvb    ->push_back(bjet->auxdata<double>("MVb_discriminant"));
+    try{
+      v_jet_mv1c   ->push_back(bjet->auxdata<double>("MV1c_discriminant"));
+      v_jet_mv2c00 ->push_back(bjet->auxdata<double>("MV2c00_discriminant"));
+      v_jet_mv2c10 ->push_back(bjet->auxdata<double>("MV2c10_discriminant"));
+      v_jet_mv2c20 ->push_back(bjet->auxdata<double>("MV2c20_discriminant"));
+      v_jet_mvb    ->push_back(bjet->auxdata<double>("MVb_discriminant"));
+    }
+    catch(...){
+      //todo: write out some warning here but don't want to clog logfiles for now
+    }
     
     // matching      
     TLorentzVector reco_bjet;
