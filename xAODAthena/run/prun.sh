0a4a87c2a8cf8d9301201f72dfc574add6a1662a
marx <marx@cern.ch>
2014-10-28 10:09:42 +0000

    correct tag

--- xAODAthena/run/prun.sh
+++ xAODAthena/run/prun.sh
@@ -10,7 +10,7 @@ cat $inputs | while read inDataSet
       #echo skipping ${inDataSet}
       continue
   fi
-  outDataSet=user.marx.test.flavntuplev6
+  outDataSet=user.marx.test.flavntuplev8
 
 
 
@@ -20,7 +20,7 @@ cat $inputs | while read inDataSet
  
 
 ##group version version
-  prun --exec "echo %IN | sed 's/,/\n/g' > input.txt; athena jobOptions.py" --athenaTag=AthAnalysisBase,2.0.14 --noBuild --inDS ${inDataSet} --outDS ${outDataSet} --nFilesPerJob 1 --excludedSite ANALY_BHAM_SL6,ANALY_ECDF_SL6
+  prun --exec "echo %IN | sed 's/,/\n/g' > input.txt; athena jobOptions.py" --athenaTag=AthAnalysisBase,2.0.14 --noBuild --inDS ${inDataSet} --outDS ${outDataSet} --nFilesPerJob 1 --excludedSite ANALY_BHAM_SL6
 #--site BNL-OSG2_DATADISK
 
 
