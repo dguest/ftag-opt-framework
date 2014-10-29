25ac81f54f4db46c62191064bd44b20b4de3deb9
marx <marx@cern.ch>
2014-10-29 21:40:22 +0000

    updating release and sample for grid submissio

--- xAODAthena/run/prun.sh
+++ xAODAthena/run/prun.sh
@@ -10,7 +10,7 @@ cat $inputs | while read inDataSet
       #echo skipping ${inDataSet}
       continue
   fi
-  outDataSet=user.marx.test.flavntuplev8
+  outDataSet=user.marx.test.flavntuplev9
 
 
 
@@ -20,7 +20,8 @@ cat $inputs | while read inDataSet
  
 
 ##group version version
-  prun --exec "echo %IN | sed 's/,/\n/g' > input.txt; athena jobOptions.py" --athenaTag=AthAnalysisBase,2.0.14 --noBuild --inDS ${inDataSet} --outDS ${outDataSet} --nFilesPerJob 1 --excludedSite ANALY_BHAM_SL6
+  prun --exec "echo %IN | sed 's/,/\n/g' > input.txt; athena jobOptions.py" --athenaTag=AtlasProduction,19.1.4.1 --noBuild --inDS ${inDataSet} --outDS ${outDataSet} --nFilesPerJob 1
+#--excludedSite ANALY_BHAM_SL6
 #--site BNL-OSG2_DATADISK
 
 
