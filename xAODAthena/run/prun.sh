d693e78b2a04858f6c454e2e8104e2373664c64e
vdao <vdao@cern.ch>
2014-11-02 11:37:57 +0000

    fixing submission script

--- xAODAthena/run/prun.sh
+++ xAODAthena/run/prun.sh
@@ -10,7 +10,7 @@ cat $inputs | while read inDataSet
       #echo skipping ${inDataSet}
       continue
   fi
-  outDataSet=user.marx.test.flavntuplev9
+  outDataSet=user.vdao.flavntuplev4
 
 
 
@@ -20,11 +20,11 @@ cat $inputs | while read inDataSet
  
 
 ##group version version
-  prun --exec "echo %IN | sed 's/,/\n/g' > input.txt; athena jobOptions.py" --athenaTag=AtlasProduction,19.1.4.1 --noBuild --inDS ${inDataSet} --outDS ${outDataSet} --nFilesPerJob 1
+##  prun --exec "echo %IN | sed 's/,/\n/g' > input.txt; athena jobOptions.py" --athenaTag=AthAnalysisBase,2.0.15 --noBuild --inDS ${inDataSet} --outDS ${outDataSet} --nFilesPerJob 5
 #--excludedSite ANALY_BHAM_SL6
 #--site BNL-OSG2_DATADISK
 
-
+  pathena  jobOptions.py  --inDS ${inDataSet} --outDS ${outDataSet} --nFilesPerJob 3
 
 done
 
