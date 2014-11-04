2ad381b7235fee463887e06a52b76ee2043a4ed4
vdao <vdao@cern.ch>
2014-11-04 15:57:45 +0000

    add submit script

--- xAODAthena/run/jobOptions.py
+++ xAODAthena/run/jobOptions.py
@@ -1,6 +1,7 @@
 # ====================================================================
 # Specify input files
 # ====================================================================
+
 theApp.EvtMax = -1
 import AthenaPoolCnvSvc.ReadAthenaPool
 # define folders where samples are located
@@ -11,120 +12,15 @@ ttbar13folder2 = mainfolder + "ttbar/13TeV/mc14_13TeV.110401.PowhegPythia_P2012_
 ttbar13folder3 = mainfolder + "ttbar/13TeV/mc14_13TeV.110401.PowhegPythia_P2012_ttbar_nonallhad.recon.AOD.e2928_s1982_s2008_r5787_tid01587950_00/"
 Zprime13folder = mainfolder + "Zprime/13TeV/mc14_13TeV.110903.Pythia8_AU2MSTW2008LO_zprime1000_tt.merge.AOD.e2743_s1982_s2008_r5787_r5853_tid01598032_00/"
 # define input collection
+
+mainfolder = "/afs/cern.ch/work/v/vdao/xAODs/"
+foldername = mainfolder + "mc14_8TeV.117050.PowhegPythia_P2011C_ttbar.merge.AOD.e1727_s1933_s1911_r5778_r5625_tid01571944_00/"
+
+import glob
+svcMgr.EventSelector.InputCollections = glob.glob(foldername+"*.root*")
+
 svcMgr.EventSelector.InputCollections = [
-    # ttbar 8TeV
-    #ttbar8folder + "AOD.01571944._010058.pool.root.1",
-    # ttbar 13TeV 5787
-    #mainfolder + "ttbar/13TeV/AOD.01597980._000008.pool.root.1",
-    #mainfolder + "ttbar/13TeV/AOD.01597980._000019.pool.root.1"
-    ttbar13folder1 + "AOD.01587947._000270.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._000291.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._000420.pool.root.2",
-    ## ttbar13folder1 + "AOD.01587947._000501.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._000673.pool.root.2",
-    ## ttbar13folder1 + "AOD.01587947._000841.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._000907.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._001089.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._001193.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._001289.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._001663.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._001699.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._002031.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._002215.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._002240.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._002328.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._002378.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._002541.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._002840.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._002848.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._002891.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._003593.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._003650.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._003703.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._004120.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._004301.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._004608.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._004689.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._004736.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._004887.pool.root.1",
-    ## ttbar13folder1 + "AOD.01587947._004933.pool.root.3",
-    ## ttbar13folder1 + "AOD.01587947._004948.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._010051.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._010064.pool.root.3",
-    ## ttbar13folder2 + "AOD.01587949._010164.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._010208.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._010269.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._010766.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._011022.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._011077.pool.root.4",
-    ## ttbar13folder2 + "AOD.01587949._011409.pool.root.2",
-    ## ttbar13folder2 + "AOD.01587949._011491.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._011695.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._011901.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._012038.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._012202.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._012218.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._012315.pool.root.2",
-    ## ttbar13folder2 + "AOD.01587949._012496.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._012541.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._012543.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._012625.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._012863.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._013416.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._013683.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._013815.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._014172.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._014202.pool.root.2",
-    ## ttbar13folder2 + "AOD.01587949._014216.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._014310.pool.root.2",
-    ## ttbar13folder2 + "AOD.01587949._014729.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._014773.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._014794.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._014843.pool.root.1",
-    ## ttbar13folder2 + "AOD.01587949._014913.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._015046.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._015128.pool.root.2",
-    ## ttbar13folder3 + "AOD.01587950._015139.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._015144.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._015558.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._015820.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._015993.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._016263.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._016341.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._016375.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._016494.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._016786.pool.root.2",
-    ## ttbar13folder3 + "AOD.01587950._016908.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._016932.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._016962.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._017065.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._017095.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._017096.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._017265.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._017347.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._017380.pool.root.5",
-    ## ttbar13folder3 + "AOD.01587950._017556.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._017745.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._017765.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._018320.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._018504.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._018575.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._018805.pool.root.2",
-    ## ttbar13folder3 + "AOD.01587950._019162.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._019297.pool.root.3",
-    ## ttbar13folder3 + "AOD.01587950._019365.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._019387.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._019692.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._019712.pool.root.1",
-    ## ttbar13folder3 + "AOD.01587950._019856.pool.root.2",
-    ## ttbar13folder3 + "AOD.01587950._019927.pool.root.1"
-    # Zprime
-    ## Zprime13folder + "AOD.01598032._000007.pool.root.1",
-    ## Zprime13folder + "AOD.01598032._000008.pool.root.1",
-    ## Zprime13folder + "AOD.01598032._000097.pool.root.1",
-    ## Zprime13folder + "AOD.01598032._000126.pool.root.1",
-    ## Zprime13folder + "AOD.01598032._000151.pool.root.1",
-    ## Zprime13folder + "AOD.01598032._000197.pool.root.1",
+     "/afs/cern.ch/work/v/vdao//xAODs/mc14_13TeV.110401.PowhegPythia_P2012_ttbar_nonallhad.recon.AOD.e2928_s1982_s2008_r5787_tid01587947_00/AOD.01587947._004222.pool.root.1",
      ]
 # ====================================================================
 # Define output ntuple file name
@@ -140,7 +36,7 @@ algSeq = CfgMgr.AthSequencer("AthAlgSeq")
 # ====================================================================
 #algSeq += CfgMgr.btagIBLAnalysisAlg("MyTry", RootStreamName = streamName, RootDirName = "/Histograms", OutputLevel=INFO) #DEBUG
 alg = CfgMgr.btagIBLAnalysisAlg(OutputLevel=INFO) #DEBUG
-alg.JetCleaningTool.CutLevel = "LooseBad" # options: "VeryLooseBad","LooseBad","MediumBad","TightBad"
+######alg.JetCleaningTool.CutLevel = "LooseBad" # options: "VeryLooseBad","LooseBad","MediumBad","TightBad"
 algSeq += alg
 ToolSvc += CfgMgr.JetCalibrationTool("JetCalibrationTool", IsData=False,ConfigFile="JetCalibTools/data/CalibrationConfigs/JES_Full2012dataset_Preliminary_MC14.config", CalibSequence="EtaJES", JetCollection="AntiKt4LCTopo")
 # ====================================================================
---
+++ xAODAthena/run/submitJobs.py
@@ -0,0 +1,42 @@
+import os
+import sys
+
+dsList=open("mc_samples.txt",'r')
+lines=dsList.readlines()
+
+suffix=".BTAGNTUP_V0"
+
+def submitJob(ds) :
+    com = "pathena  jobOptions.py "
+    com += "--inDS " + ds + " "
+
+    oDS = "user.vdao."+ds.replace("/","")+suffix
+    print oDS+"  has length: "+str(len(oDS))
+    while len(oDS) > 115 :
+        print len(oDS)," too long!!!"
+        splODS=oDS.split("_")
+        splODS.pop(2)
+        oDS="_".join(splODS)
+        pass
+    #oDS=oDS.replace("_AUET2B_MSTW2008LO_","")
+    #oDS=oDS.replace("ProtosPythia","PP")
+    #oDS=oDS.replace("group.perf-idtracking","perf-id")
+    print "final: "+oDS+"  has length: "+str(len(oDS))
+    com += "--outDS "+ oDS + " "
+  
+    com += "--nFilesPerJob 3 "
+    com += "--destSE UNI-FREIBURG_SCRATCHDISK "
+    return com
+
+for ds in lines :
+    if "#" in ds : continue
+    if "mc" not in ds: continue
+
+    print " "
+    print "///////////////////////////////////////////////////////////////////////////////////////"
+    print ds
+    command=submitJob(ds.replace("\n",""))
+    print command
+    os.system(command)
+    pass
+
