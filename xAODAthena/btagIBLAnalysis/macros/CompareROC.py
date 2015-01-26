abda235bfc5572a4e3a59b17dfd8df04b47160b2
vdao <vdao@cern.ch>
2015-01-26 14:06:14 +0000

    more macros

--- xAODAthena/btagIBLAnalysis/macros/CompareROC.py
+++ xAODAthena/btagIBLAnalysis/macros/CompareROC.py
@@ -55,12 +55,10 @@ while obj!=None:
     if "---bc" in name:
         pass
     else:
-        if "MVb" not in name: 
+        if "MVb" not in name and "MV1" not in name: 
             taggerList.append( name.split("---")[0] )
     obj=iterator()
 print taggerList
-
-
 
 light=TH1F("b VS light","b VS light",100,0.3,1);
 light.SetTitle(";b-jet efficiency;light-jet rejection;")
@@ -91,14 +89,18 @@ legend4.SetFillStyle(0)
 legend4.SetBorderSize(0)
 count=1
 for tag in taggerList:
+    print tag
     myC.cd()
     f1.cd()
     curve=f1.Get(tag+"---bl")
     f2.cd()
     curve2=f2.Get(tag+"---bl")
     if curve2==None: continue
+    print curve
+    print curve2
     count+=1
     if count==5: count+=1
+    if count==9: count=kOrange
     curve2.SetLineStyle(2)
     curve.SetLineColor(count)
     curve2.SetLineColor(count)
@@ -109,18 +111,27 @@ for tag in taggerList:
     legend4.AddEntry(curve ,tag.replace("_","+")+" "+leg1,"L")
     legend4.AddEntry(curve2,tag.replace("_","+")+" "+leg2,"L")
 
-    myCx=TCanvas( "bVSl"+tag, "bVSl"+tag,900,900);
-    myCx.SetLogy()
-    myCx.SetGridy()
-    myCx.SetGridx()
+    myCx=TCanvas( "bVSl"+tag, "bVSl"+tag,800,800);
+    pad_1=TPad("pad_1", "up", 0., 0.35, 1., 1.);
+    pad_1.SetBottomMargin(0);
+    pad_1.Draw();   
+    pad_1.SetGridy()
+    pad_1.SetGridx()
+    pad_1.SetLogy()
+    pad_2=TPad("pad_2", "down", 0.0, 0.00, 1.0, 0.35);
+    pad_2.SetTopMargin(0);
+    pad_2.SetBottomMargin(0.28);
+    pad_2.Draw();
+    pad_1.cd();
+
     light.Draw()
     myText(0.20,0.24,1,myLumi,0.045)
     myText(0.20,0.19,1,myLumi2,0.045)
     curve.Draw("C")
     curve2.Draw("C")
-    legend5=TLegend(0.60,0.80,0.92,0.90)
+    legend5=TLegend(0.55,0.77,0.92,0.90)
     legend5.SetTextFont(42)
-    legend5.SetTextSize(0.038)
+    legend5.SetTextSize(0.042)
     legend5.SetFillColor(0)
     legend5.SetLineColor(0)
     legend5.SetFillStyle(0)
@@ -128,6 +139,45 @@ for tag in taggerList:
     legend5.AddEntry(curve ,tag.replace("_","+")+" "+leg1,"L")
     legend5.AddEntry(curve2,tag.replace("_","+")+" "+leg2,"L")
     legend5.Draw("SAME")
+
+    pad_2.cd()
+    ratio=light.Clone("ratio")
+    ratio.GetYaxis().SetTitle("rel. diff.")
+    ratio.GetYaxis().SetTitleOffset(1.1)
+    ratio.GetXaxis().SetLabelSize(0.10)
+    ratio.GetXaxis().SetTitleSize(0.10)
+    ratio.GetYaxis().SetTitleOffset(0.7)
+    ratio.GetYaxis().SetLabelSize(0.09)
+    ratio.GetYaxis().SetTitleSize(0.09)
+    ratio.SetMaximum(0.8)
+    ratio.SetMinimum(-0.8)
+    ratio.SetLineColor(kGray)
+    ratio.Draw("HIST")
+
+    ratC=curve2.Clone("ratioC")
+    for bin in xrange(1,ratC.GetN()+1):
+        px1=Double(0)
+        py1=Double(0)
+        curve.GetPoint(bin-1,px1,py1)
+        if py1==0: continue
+        print str(bin)+" , "+str(px1)+" "+str(py1)
+        clo_py2=100000
+        clo_px2=100000
+        for bin2 in xrange(1,ratC.GetN()+1):
+            px2=Double(0)
+            py2=Double(0)
+            curve2.GetPoint(bin2-1,px2,py2)
+            if fabs(px2-px1)<fabs(clo_px2-px1):
+                clo_py2=py2
+                clo_px2=px2
+
+        ratC.SetPoint(bin-1,px1,clo_py2/py1-1)
+                
+    ratC.SetLineWidth(4)
+    ratC.SetLineColor(1)
+    ratC.Draw("L")
+    
+    myCx.Update()
     myCx.Print(odir+"/bVSlight__"+tag+".eps")
 
 myC.cd()
@@ -135,8 +185,7 @@ legend4.Draw()
 myText(0.20,0.24,1,myLumi,0.045)
 myText(0.20,0.19,1,myLumi2,0.045)
 myC.Update()
-time.sleep(1)
-myC.Print(odir+"/bVSlightALL.eps")
+##myC.Print(odir+"/bVSlightALL.eps")
 
 
 ################################################################
@@ -160,6 +209,7 @@ for tag in taggerList:
     if curve2==None: continue
     count+=1
     if count==5: count+=1
+    if count==9: count=kOrange
     curve2.SetLineStyle(2)
     curve.SetLineColor(count)
     curve2.SetLineColor(count)
@@ -168,16 +218,25 @@ for tag in taggerList:
     curve.Draw("C")
     curve2.Draw("C")
     
-    myCx=TCanvas( "bVSc"+tag, "bVSc"+tag,900,900);
-    myCx.SetLogy()
-    myCx.SetGridy()
-    myCx.SetGridx()
+    myCx=TCanvas( "bVSc"+tag, "bVSc"+tag,800,800);
+    pad_1=TPad("pad_1", "up", 0., 0.35, 1., 1.);
+    pad_1.SetBottomMargin(0);
+    pad_1.Draw();   
+    pad_1.SetGridy()
+    pad_1.SetGridx()
+    pad_1.SetLogy()
+    pad_2=TPad("pad_2", "down", 0.0, 0.00, 1.0, 0.35);
+    pad_2.SetTopMargin(0);
+    pad_2.SetBottomMargin(0.28);
+    pad_2.Draw();
+    pad_1.cd();
+    
     cj.Draw()
     myText(0.20,0.24,1,myLumi,0.045)
     myText(0.20,0.19,1,myLumi2,0.045)
     curve.Draw("C")
     curve2.Draw("C")
-    legend5=TLegend(0.60,0.80,0.92,0.90)
+    legend5=TLegend(0.55,0.77,0.92,0.90)
     legend5.SetTextFont(42)
     legend5.SetTextSize(0.038)
     legend5.SetFillColor(0)
@@ -187,6 +246,46 @@ for tag in taggerList:
     legend5.AddEntry(curve ,tag.replace("_","+")+" "+leg1,"L")
     legend5.AddEntry(curve2,tag.replace("_","+")+" "+leg2,"L")
     legend5.Draw("SAME")
+
+    pad_2.cd()
+    ratio=cj.Clone("ratio")
+    ratio.GetYaxis().SetTitle("rel. diff.")
+    ratio.GetYaxis().SetTitleOffset(1.1)
+    ratio.GetXaxis().SetLabelSize(0.10)
+    ratio.GetXaxis().SetTitleSize(0.10)
+    ratio.GetYaxis().SetTitleOffset(0.7)
+    ratio.GetYaxis().SetLabelSize(0.09)
+    ratio.GetYaxis().SetTitleSize(0.09)
+    ratio.SetMaximum(0.8)
+    ratio.SetMinimum(-0.8)
+    ratio.SetLineColor(kGray)
+    ratio.Draw("HIST")
+
+    ratC=curve2.Clone("ratioC")
+    for bin in xrange(1,ratC.GetN()+1):
+        px1=Double(0)
+        py1=Double(0)
+        curve.GetPoint(bin-1,px1,py1)
+        if py1==0: continue
+        print str(bin)+" , "+str(px1)+" "+str(py1)
+        clo_py2=100000
+        clo_px2=100000
+        for bin2 in xrange(1,ratC.GetN()+1):
+            px2=Double(0)
+            py2=Double(0)
+            curve2.GetPoint(bin2-1,px2,py2)
+            if fabs(px2-px1)<fabs(clo_px2-px1):
+                clo_py2=py2
+                clo_px2=px2
+
+        ratC.SetPoint(bin-1,px1,clo_py2/py1-1)
+                
+    ratC.SetLineWidth(4)
+    ratC.SetLineColor(1)
+    ratC.Draw("L")
+    myCx.Update()
+
+
     myCx.Print(odir+"/bVSc__"+tag+".eps")
 
 myC2.cd()
@@ -194,8 +293,5 @@ legend4.Draw()
 myText(0.20,0.24,1,myLumi,0.045)
 myText(0.20,0.19,1,myLumi2,0.045)
 myC2.Update()
-time.sleep(1)
-myC2.Print(odir+"/bVScALL.eps")
+##myC2.Print(odir+"/bVScALL.eps")
 
-
-###time.sleep(111111)
