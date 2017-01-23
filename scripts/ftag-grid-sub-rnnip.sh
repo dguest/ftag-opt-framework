21a2c8e2c6c9939fcd1deec00f4615dcb314c6aa
Dan Guest <dguest@cern.ch>
2017-01-23 22:04:21 +0100

    switch to r21 sample

--- scripts/ftag-grid-sub-rnnip.sh
+++ scripts/ftag-grid-sub-rnnip.sh
@@ -10,7 +10,7 @@ fi
 
 SCRIPT_DIR=$(dirname $BASH_SOURCE)
 JO=jobOptions_ipmp.py
-DS=mc15_13TeV:mc15_13TeV.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.merge.DAOD_FTAG2.e3698_s2608_s2183_r7725_r7676_p2625/
+DS=mc16_13TeV.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.merge.AOD.e3698_s2997_r8903_r8906/
 
 ${SCRIPT_DIR}/ftag-grid-sub.sh -j $JO -d $DS -u
 
